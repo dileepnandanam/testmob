64eca85a2a5d9a9f20b6d42dcb61e979cb2b1ddb
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-29 21:35:24 +0530

    preparing front end in rails way

--- app/controllers/test_induviduals_controller.rb
+++ app/controllers/test_induviduals_controller.rb
@@ -1,35 +1,35 @@
 class TestInduvidualsController < ApplicationController
   
   def advanced_mode
-  
+    render 'test_induvidual_common_template'
   end
 
   def basic_motion
-  
+    render 'test_induvidual_common_template'
   end
 
   def connections
-  
+    render 'test_induvidual_common_template'
   end
 
   def developer
-  
+    render 'test_induvidual_common_template'
   end
 
   def interpolated_mode
-  
+    render 'test_induvidual_common_template'
   end
 
   def others
-  
+    render 'test_induvidual_common_template'
   end
 
   def power_control
-  
+    render 'test_induvidual_common_template'
   end
 
   def rig_control
-  
+    render 'test_induvidual_common_template'
   end
 
 end
