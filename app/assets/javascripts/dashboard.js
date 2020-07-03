1f11f11d51de2b1568c1ab706671f61018086880
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-07-03 11:23:48 +0530

    minimul ui

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -7,4 +7,9 @@ $(document).on('turbolinks:load', function() {
     $('.result').html('Executing...')
   })
 
+  $(document).on('ajax:success', '.nav-link', function(e) {
+    $('.interactive-ui').replaceWith(e.detail[2].responseText)
+    addSubmitEvent()
+  })
+
 })
--- app/controllers/test_induviduals_controller.rb
+++ app/controllers/test_induviduals_controller.rb
@@ -1,35 +1,35 @@
 class TestInduvidualsController < ApplicationController
   before_action :check_user
   def advanced_mode
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def basic_motion
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def connections
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def developer
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def interpolated_mode
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def others
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def power_control
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   def rig_control
-    render 'test_induvidual_common_template'
+    render 'test_induvidual_common_template', layout: false
   end
 
   protected
