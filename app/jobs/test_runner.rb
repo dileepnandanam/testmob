6aa2952c7b1a9e18d3b9d85c36d5990656b06904
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-07-07 12:53:33 +0530

    delay

--- app/jobs/test_runner.rb
+++ app/jobs/test_runner.rb
@@ -4,7 +4,12 @@ class TestRunner < ApplicationJob
   def perform(test_id) 
     @test = Test.find(test_id)
     @test.code.split(/\n/).each do |line|
-      Quaco.execute(@test.user_id, line) if line.present?
+      if line.starts_with?('delay')
+        OutputSender.perform_later(@test.user_id, line, 1)
+        sleep(line.split(':').last.to_f)
+      else
+        Quaco.execute(@test.user_id, line) if line.present?
+      end
     end
   end
 end
