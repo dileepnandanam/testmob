6262c21757212da94b1bd4c11abb264b9039d823
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-07-08 22:05:41 +0530

    few tweaks

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'quaco.web@gmail.com'
   def inbound_demo_request
     @user = params[:user]
-    mail(to: simple_form_for(AppConfig.where(name: 'sales_email').first, subject: 'New Demo Request')
+    mail(to: AppConfig.where(name: 'sales_email').first, subject: 'New Demo Request')
   end
 end
