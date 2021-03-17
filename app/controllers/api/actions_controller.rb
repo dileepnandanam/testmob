de718e251b55cfcb2bfc9865911b9ab75518b5ab
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-03-17 19:46:26 +0530

    useing session if auth not given

--- app/controllers/api/actions_controller.rb
+++ app/controllers/api/actions_controller.rb
@@ -25,7 +25,7 @@ class Api::ActionsController < ApplicationController
   end
 
   def check_user!
-    user = current_user
+    user = current_user_from_token_or_session
     unless user && ['client', 'platform'].include?(user.usertype)
       render json: {message: 'Access denied'}, status: 401 and return
     end
@@ -48,10 +48,10 @@ class Api::ActionsController < ApplicationController
     end
   end
 
-  def current_user
+  def current_user_from_token_or_session
     access_token = params[:access_token] || request.headers['access_token']
     email = params[:email] || request.headers['email']
     user = User.where(access_token: Digest::SHA1.hexdigest(access_token.to_s), email: email).first
-    user
+    user || current_user
   end
 end
