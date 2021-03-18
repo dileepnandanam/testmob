cfb692ce9230967fb8c676f4a12b89b0bf835e9a
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-03-18 16:42:36 +0530

    mvp for crop

--- app/assets/javascripts/interactive_tests.js
+++ app/assets/javascripts/interactive_tests.js
@@ -1,3 +1,5 @@
+var crop = null
+
 $(document).on('turbolinks:load', () => {
   $(document).on('ajax:success', '.refresh-vision', (e) => {
     $('.vision-screenshot').attr('src', e.detail[0].screen_shot)
@@ -12,4 +14,14 @@ $(document).on('turbolinks:load', () => {
   })
 
   
+  crop = (x1,y1,x2,y2) => {
+    var height = y2-y1
+    var width = x2-x1
+    var aspect_ratio = parseFloat(width)/parseFloat(height)
+    $('.crop-result').css('height', 300/aspect_ratio+'px')
+    var corped_image = $('.crop-result')[0]
+    var canvas_context = corped_image.getContext("2d")
+    var vision_image = $('.vision-screenshot')[0]
+    canvas_context.drawImage(vision_image, x1, y1, width, height, 0, 0, 300, 300/aspect_ratio)
+  }
 })
