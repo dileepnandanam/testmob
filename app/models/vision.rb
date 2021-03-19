6d8410c06387376116af5c8c938f9c67ea8ea8a2
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-03-19 23:22:20 +0530

    integrating python server for image ops

--- app/models/vision.rb
+++ app/models/vision.rb
@@ -1,20 +1,22 @@
 require 'base64'
 class Vision
   def execute_touch(image)
-    return(image)
+    return `curl --form "croped_image=#{image}" -X POST localhost:8080/execute_touch`
   end
   
   def execute_predefined_actions
-    return(capture)
+    return `curl -X POST localhost:8080/execute_predefined_actions`
   end
   
   def execute_text_command(text)
-    return(capture)
+    return `curl --form "text=#{text}" -X POST localhost:8080/execute_touch`
   end
   
   def capture
-    File.open(Rails.root.join('vis.bmp'), 'rb') do |img|
-      'data:image/bmp;base64,' + Base64.strict_encode64(img.read)
-    end
+    #Uncomment this for development
+    #File.open(Rails.root.join('vis.bmp'), 'rb') do |img|
+    #  'data:image/bmp;base64,' + Base64.strict_encode64(img.read)
+    #end
+    return `curl localhost:8080/capture`
   end
 end
