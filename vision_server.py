ae448acb0ac30dcf9eb335e424dc8721e79d6a7e
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-03-20 17:51:17 +0530

    reading from cam

--- vision_server.py
+++ vision_server.py
@@ -4,6 +4,13 @@ import cgi
 from cgi import parse_header, parse_multipart
 import base64
 
+from pypylon import pylon
+from PIL import Image as im
+import cv2
+from io import BytesIO
+
+
+
 #interactive console
 #import code; code.interact(local=dict(globals(), **locals()))
 
@@ -12,10 +19,30 @@ hostName = "localhost"
 serverPort = 8080
 
 
+#Globals for Camera
+img = pylon.PylonImage()
+
+
 #TO DO
 def capture():
   print("CAPTURING SCREENSHOT")
-  return SAMPLE_IMAGE
+
+  cam.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+  converter = pylon.ImageFormatConverter()
+  # converting to opencv bgr format
+  converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+  converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+  grabResult = cam.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+  # Access the image data
+  image = converter.Convert(grabResult)
+  img = image.GetArray()
+  pil_img = im.fromarray(img)
+  buff = BytesIO()
+  pil_img.save(buff, format="JPEG")
+  encoded_img = base64.b64encode(buff.getvalue()).decode("utf-8")
+  cam.StopGrabbing()
+
+  return encoded_img
 
 def execute_touch(croped_image_base64):
   print("EXECUTING TOUCH")
@@ -27,6 +54,8 @@ def execute_text_command(command):
 
 def execute_predefined_actions():
   print("EXECUTING PREDEFINED ACTIONS")
+
+
 
 
 #SERVER, DO NOT CHANGE
@@ -64,14 +93,20 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_image_response(capture())   
 
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+if __name__ == "__main__":
+  
+  #camera....................................
+  pylon_cam = pylon.TlFactory.GetInstance()
+  cam = pylon.InstantCamera(pylon_cam.CreateFirstDevice())
+  cam.Open()
+  #...........................................
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+  webServer = HTTPServer((hostName, serverPort), MyServer)
+  print("Server started http://%s:%s" % (hostName, serverPort))
+  try:
+    webServer.serve_forever()
+  except KeyboardInterrupt:
+    pass
+  webServer.server_close()
+  cam.Close()
+  print("Server stopped.")
