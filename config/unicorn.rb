ff4c3e2779fa68b0cf901b1868f58a2c5abd7ed3
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-03-26 20:28:40 +0530

    unicorn worker timeout

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ working_directory app_dir
 # Set unicorn options
 worker_processes 1
 preload_app true
-timeout 30
+timeout 300
 
 # Set up socket location
 listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
