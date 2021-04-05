7d5ba06d2c445e8482035dbe9bef657ddccd1519
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-04-05 13:25:46 +0530

    try different

--- vision_server/vision_server.py
+++ vision_server/vision_server.py
@@ -118,7 +118,7 @@ def get_real_coordinates(data):
 def get_coordinates_from_image():
     input_image = cv2.imread('/tmp/vision_input.jpeg', 0)
     vision_image = cv2.imread('/tmp/vision_output.jpeg')
-    return(find_match(vision_image, input_image))
+    return(match_image(vision_image, input_image))
 
 def get_coordinates_from_command(command):
     return(None)
