64e80088535bb4bf8815aaf9dc074f160f087e8b
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-04-05 10:16:06 +0530

    show all

--- vision_server/match_template.py
+++ vision_server/match_template.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 
+def midpoint(matched):
+    x = (matched[1][0] - matched[2][0])/2
+    y = (matched[1][1] - matched[2][1])/2
+    return((x,y))
+
 def find_match(big_image, small_image):
     img_gray = cv2.cvtColor(big_image, cv2.COLOR_BGR2GRAY)
     w, h = small_image.shape[::-1]
@@ -11,7 +16,7 @@ def find_match(big_image, small_image):
     for pt in zip(*loc[::-1]):
         matched = pt, (pt[0] + w, pt[1] + h)
         point1,point2 = pt, (pt[0] + w, pt[1] + h)
-    cv2.rectangle(big_image, point1, point2, (0,0,255), 2)
+        cv2.rectangle(big_image, point1, point2, (0,0,255), 2)
 
     cv2.imwrite('/tmp/vision_output.jpeg',big_image)
-    return(matched)
+    return(midpoint(matched))
