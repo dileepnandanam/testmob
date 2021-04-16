bcf5c694107820471bbee54400a7d5a5c6262093
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-04-16 14:52:33 +0530

    fix header

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -11,7 +11,7 @@ class Api::BaseController < ApplicationController
   end
 
   def current_user_from_token_or_session
-    access_token = params[:access_token] || request.headers['access_token']
+    access_token = params[:access_token] || request.headers['access-token']
     email = params[:email] || request.headers['email']
     user = User.where(access_token: Digest::SHA1.hexdigest(access_token.to_s), email: email).first
     user || current_user
